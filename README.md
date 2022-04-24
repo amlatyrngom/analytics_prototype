2f5f732b38c09d5f5543a44ad64ef5772e3df3be
Amadou Ngom <amlatyrngom@protonmail.com>
2022-04-24 12:32:52 -0400

    Running expts

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # Smart IDs
 Repo for Smart IDs.
 
-## Current Features
-* Table Storage.
-* Loading tables (including TPCH tables) from csv.
-* Simple Vectorized Query Engine: Scan, Filter, Projection, Joins, Aggregations.
-
-## Next Features
-* Query Engine Features:
-  * Parallel Execution.
-  * Indexes.
-  * NULL support.
-* Parser.
-* Optimizer & Cost Model.
--- libs.sh
+++ libs.sh
@@ -1,5 +1,9 @@
-sudo apt install clang-11
-sudo apt install openssl
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
+
+sudo apt install -y clang-11 openssl zip unzip build-essential
 wget https://github.com/Kitware/CMake/releases/download/v3.21.0-rc1/cmake-3.21.0-rc1.tar.gz
 tar -xvf cmake-3.21.0-rc1.tar.gz
 cd cmake-3.21.0-rc1/
@@ -8,7 +12,22 @@ make
 make install
 cd ..
 
+cd job_light_workload
+wget https://muimages.sfo2.digitaloceanspaces.com/job_light_trimmed_data.zip
+unzip job_light_trimmed_data.zip
+cd ..
+
 export CC=clang-11
 export CXX=clang++-11
+
+mkdir cmake-debug
+cd cmake-debug
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+cd ..
+
+mkdir cmake-release
+cd cmake-release
 cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cd ..
+
+
